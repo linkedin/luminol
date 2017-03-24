8ea00091e8244912f939d4edba8adf6f81abd707
brennv <brennv@users.noreply.github.com>
2017-03-24 11:05:27 -0700

    2 --> 4 spaces

--- src/luminol/modules/anomaly.py
+++ src/luminol/modules/anomaly.py
@@ -16,29 +16,29 @@ Anomaly Object
 
 class Anomaly(object):
 
-  def __init__(self, start_timestamp, end_timestamp, anomaly_score, exact_timestamp):
-    """
-    Construct an anomaly object.
-    :param:start_timestamp: start time of the anomaly period.
-    :param:end_timestamp: end time of the anomaly period.
-    :param:anomly_score: the score of the anomaly.
-    :param:exact_timestamp: the timestamp within the period where the anomaly likely happened.
-    """
-    self.start_timestamp = start_timestamp
-    self.end_timestamp = end_timestamp
-    self.anomaly_score = anomaly_score
-    self.exact_timestamp = exact_timestamp
+    def __init__(self, start_timestamp, end_timestamp, anomaly_score, exact_timestamp):
+        """
+        Construct an anomaly object.
+        :param:start_timestamp: start time of the anomaly period.
+        :param:end_timestamp: end time of the anomaly period.
+        :param:anomly_score: the score of the anomaly.
+        :param:exact_timestamp: the timestamp within the period where the anomaly likely happened.
+        """
+        self.start_timestamp = start_timestamp
+        self.end_timestamp = end_timestamp
+        self.anomaly_score = anomaly_score
+        self.exact_timestamp = exact_timestamp
 
-  def get_time_window(self):
-    """
-    Get the anomaly period.
-    :return tuple: a tuple representation of the anomaly period.
-    """
-    return self.start_timestamp, self.end_timestamp
+    def get_time_window(self):
+        """
+        Get the anomaly period.
+        :return tuple: a tuple representation of the anomaly period.
+        """
+        return self.start_timestamp, self.end_timestamp
 
-  def __str__(self):
-    """
-    return string representation of the anomaly
-    :return: string
-    """
-    return "Anomaly from {0} to {1} with score {2}".format(self.start_timestamp, self.end_timestamp, self.anomaly_score)
+    def __str__(self):
+        """
+        return string representation of the anomaly
+        :return: string
+        """
+        return "Anomaly from {0} to {1} with score {2}".format(self.start_timestamp, self.end_timestamp, self.anomaly_score)
--- src/luminol/modules/correlation_result.py
+++ src/luminol/modules/correlation_result.py
@@ -16,13 +16,13 @@ Correlation object
 
 class CorrelationResult(object):
 
-  def __init__(self, shift, coefficient, shifted_coefficient):
-    """
-    Construct a CorrelationResult object.
-    :param int shift: the amount of shift where the coefficient is obtained.
-    :param float coefficient: the correlation coefficient.
-    :param float shifted_coefficient: the correlation coefficient with shift taken into account.
-    """
-    self.shift = shift
-    self.coefficient = coefficient
-    self.shifted_coefficient = shifted_coefficient
+    def __init__(self, shift, coefficient, shifted_coefficient):
+        """
+        Construct a CorrelationResult object.
+        :param int shift: the amount of shift where the coefficient is obtained.
+        :param float coefficient: the correlation coefficient.
+        :param float shifted_coefficient: the correlation coefficient with shift taken into account.
+        """
+        self.shift = shift
+        self.coefficient = coefficient
+        self.shifted_coefficient = shifted_coefficient
