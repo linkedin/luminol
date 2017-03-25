8bb182392b492a23dbb576bea54823870dc4af40
brennv <brennv@users.noreply.github.com>
2017-03-24 18:20:24 -0700

    fix import

--- src/luminol/algorithms/anomaly_detector_algorithms/default_detector.py
+++ src/luminol/algorithms/anomaly_detector_algorithms/default_detector.py
@@ -9,8 +9,8 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """
-from exp_avg_detector import ExpAvgDetector
-from derivative_detector import DerivativeDetector
+from luminol.algorithms.anomaly_detector_algorithms.exp_avg_detector import ExpAvgDetector
+from luminol.algorithms.anomaly_detector_algorithms.derivative_detector import DerivativeDetector
 from luminol.algorithms.anomaly_detector_algorithms import AnomalyDetectorAlgorithm
 from luminol.constants import *
 from luminol.modules.time_series import TimeSeries
