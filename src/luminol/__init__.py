5ead17f5763e3f54c5485c2a81388cd79d12daa6
Szymon Jachim <szymonj@qualtrics.com>
2019-10-23 15:19:58 +0200

    Making 'import luminol' actually usable and compatible with README example

--- src/luminol/__init__.py
+++ src/luminol/__init__.py
@@ -10,6 +10,10 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """
 
+
+import luminol.anomaly_detector 
+import luminol.correlator
+
 from luminol import exceptions
 
 
